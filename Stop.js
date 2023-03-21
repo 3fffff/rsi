7520eaac4e9226a71766b572cd0bada719564915
3fffff <g3g3g3g3g33@yandex.ru>
2023-03-21 23:51:53 +0500

    Update Stop.js

--- Stop.js
+++ Stop.js
@@ -1,1 +1,20 @@
+let shutDownSignal = false;
 
+process
+  .on('SIGTERM', shutdown('SIGTERM'))
+  .on('SIGINT', shutdown('SIGINT'))
+  .on('uncaughtException', shutdown('uncaughtException'));
+
+function shutdown(signal) {
+    return (err) => {
+        shutDownSignal = true;
+        console.log(`Received signal: ${ signal }...`);
+        if (err) console.error(err.stack || err);
+        setTimeout(() => {
+            console.log('...waited 15s, exiting.');
+            process.exit(err ? 1 : 0);
+        }, 15000).unref();
+    };
+}
+
+export function getShutDownSingnal() { return shutDownSignal;}
